5b914274f06446ea13438fbdfe4063c16a9934ce
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-03-22 21:34:25 +0900

    Auto Layout 설정

--- MyTodoList/MyTodoList/MyTodoListViewController.swift
+++ MyTodoList/MyTodoList/MyTodoListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MyTodoListViewController.swift
 //  MyTodoList
 //
 //  Created by 유림 on 3/22/24.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MyTodoListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
 }
-
