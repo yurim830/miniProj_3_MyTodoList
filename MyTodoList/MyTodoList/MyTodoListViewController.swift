f4f88c47d4077764fb7ce36ff855cda975619c65
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-03-22 22:13:51 +0900

    DataSource 설정 - 행 개수 설정

--- MyTodoList/MyTodoList/MyTodoListViewController.swift
+++ MyTodoList/MyTodoList/MyTodoListViewController.swift
@@ -13,11 +13,26 @@ class MyTodoListViewController: UIViewController {
         TodoModel(id: 1, title: "밥먹기", isDone: true),
         TodoModel(id: 1, title: "Lv1 완성하기", isDone: false)
     ]
+    
     @IBOutlet weak var TableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        TableView.dataSource = self
     }
 
+}
+
+
+extension MyTodoListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList.count            // 행 개수 리턴
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 }
