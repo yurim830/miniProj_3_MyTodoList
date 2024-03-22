0f7fabdc32dd4237ee6d8d3254bdb21d11bc9e75
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-03-22 22:44:14 +0900

    Custom Cell을 코드에 연결하기 ( + reusable identifier 설정)

---
+++ MyTodoList/MyTodoList/MyTodoListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyTodoListTableViewCell.swift
+//  MyTodoList
+//
+//  Created by 유림 on 3/22/24.
+//
+
+import UIKit
+
+class MyTodoListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
