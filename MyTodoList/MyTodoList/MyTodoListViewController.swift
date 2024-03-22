3e72b696964ca01556ebe24c4ac16b0277775d36
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-03-22 22:02:42 +0900

    데이터 struct 생성

--- MyTodoList/MyTodoList/MyTodoListViewController.swift
+++ MyTodoList/MyTodoList/MyTodoListViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class MyTodoListViewController: UIViewController {
-
+    
+    let todoList: [TodoModel] = [
+        TodoModel(id: 1, title: "밥먹기", isDone: true),
+        TodoModel(id: 1, title: "Lv1 완성하기", isDone: false)
+    ]
+    @IBOutlet weak var TableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
---
+++ MyTodoList/MyTodoList/TodoModel.swift
@@ -0,0 +1,14 @@
+//
+//  TodoModel.swift
+//  MyTodoList
+//
+//  Created by 유림 on 3/22/24.
+//
+
+import Foundation
+
+struct TodoModel {
+    let id: Int
+    let title: String
+    let isDone: Bool
+}
